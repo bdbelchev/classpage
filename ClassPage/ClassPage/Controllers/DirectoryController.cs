4d08d1aa87685078b286fe0659f0b38deaa9efb6
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2022-03-05 17:34:44 +0200

    Added directory functionality

--- ClassPage/ClassPage/Controllers/DirectoryController.cs
+++ ClassPage/ClassPage/Controllers/DirectoryController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ClassPage.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClassPage.Controllers
 {
@@ -18,7 +19,43 @@ namespace ClassPage.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.ClassList = _context.Classes.ToList();
             return View();
+        }
+
+        public IActionResult ListTeachers()
+        {
+            List<Teacher> teachers = _context.Teachers.Include(c => c.Class).Include(s => s.TeachersSubjects).ThenInclude(s => s.Subject).ToList();
+
+            return View(teachers);
+        }
+
+        public IActionResult ListStudents()
+        {
+            List<Student> students = _context.Students.Include(c => c.Class).ToList();
+
+            return View(students);
+        }
+
+        public IActionResult TeacherDetails(int teacherId)
+        {
+            Teacher teacher = _context.Teachers.Include(c => c.Class).Include(c => c.ClassesTeachers).ThenInclude(c => c.Class).Include(s => s.TeachersSubjects).ThenInclude(s => s.Subject).FirstOrDefault(t => t.Id == teacherId);
+
+            return View(teacher);
+        }
+
+        public IActionResult StudentDetails(int studentId)
+        {
+            Student student = _context.Students.Include(c => c.Class).FirstOrDefault(s => s.Id == studentId);
+
+            return View(student);
+        }
+
+        public IActionResult ClassDetails(int classId)
+        {
+            Class schoolClass = _context.Classes.Include(c => c.MainTeacher).Include(s => s.Students).Include(t => t.ClassesTeachers).ThenInclude(t => t.Teacher).ThenInclude(s => s.TeachersSubjects).ThenInclude(s => s.Subject).FirstOrDefault(c => c.Id == classId);
+
+            return View(schoolClass);
         }
     }
 }
